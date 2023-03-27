7b4a07633e681498985b82d40f286d0b9c8f0362
LEEVGS <lee.vgs@hotmail.com>
2023-03-27 12:29:51 +0200

    Added the description and open for details

--- RunescapeItemSearcher/ViewModel/DetailVM.cs
+++ RunescapeItemSearcher/ViewModel/DetailVM.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RunescapeItemSearcher.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +15,15 @@ namespace RunescapeItemSearcher.ViewModel
     {
         private Item item;
 
-        public DetailVM() { }
+        public DetailVM()
+        {
+            OpenPageCommand = new RelayCommand(OpenPage);
+        }
+        public RelayCommand OpenPageCommand { get; private set; }
+        private void OpenPage()
+        {
+            Process.Start($"https://secure.runescape.com/m=itemdb_rs/Bond/viewitem?obj={item.Id}");
+        }
         public Item Item
         {
             get { return item; }
