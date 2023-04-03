e8fa847d08fb2a07612079fa3a43e17e615854f6
LEEVGS <lee.vgs@hotmail.com>
2023-04-03 14:47:54 +0200

    Added offline repo creator

---
+++ OfflineRepoCreator/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OfflineRepoCreator
+{
+    public class Item
+    {
+        [JsonProperty(propertyName: "id")]
+        public int Id { get; set; }
+        [JsonProperty(propertyName: "type")]
+        public string Type { get; set; }
+        [JsonProperty(propertyName: "name")]
+        public string Name { get; set; }
+        [JsonProperty(propertyName: "icon")]
+        public string Icon { get; set; }
+        [JsonProperty(propertyName: "description")]
+        public string Description { get; set; }
+        [JsonProperty(propertyName: "current")]
+        public Current Current { get; set; }
+        [JsonProperty(propertyName: "member")]
+        public bool Member { get; set; }
+    }
+    public class Current
+    {
+        [JsonProperty(propertyName: "trend")]
+        public string trend { get; set; }
+        [JsonProperty(propertyName: "price")]
+        public string price { get; set; }
+    }
+}
---
+++ OfflineRepoCreator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace OfflineRepoCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            for (int i = 0; i < 42; i++)
+            {
+                Console.Clear();
+                Console.WriteLine($"Getting data {i}/42");
+                var tempItems = GetItemsAsync("a", i).Result;
+                foreach (var item in tempItems)
+                {
+                    items.Add(item);
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+            File.WriteAllText("items.json", json);
+        }
+
+        public static async Task<List<Item>> GetItemsAsync(string name, int category)
+        {
+            if (name == string.Empty)
+            {
+                name = "a";
+            }
+            List<Item> items = new List<Item>();
+            string endpoint = $"https://secure.runescape.com/m=itemdb_rs/api/catalogue/items.json?category={category}&alpha={name}&page=1";
+            string json = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(endpoint);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new HttpRequestException(response.ReasonPhrase);
+                }
+                json = await response.Content.ReadAsStringAsync();
+            }
+
+            var cardTokens = JToken.Parse(json).SelectToken("items");
+            foreach (var token in cardTokens)
+            {
+                var item = token.ToObject<Item>();
+                items.Add(item);
+            }
+            return items;
+        }
+    }
+
+}
